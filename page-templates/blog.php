d971ebec388ca4612890a0b6a6119ec03fb942b4
Achim <Achim@Achims-5k-iMac.fritz.box>
2017-09-12 13:37:37 +0200

    Moved the news to a separate page template

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -1,4 +1,13 @@
+<?php
+/**
+ * Template Name: Blog
+ */
+?>
+
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<!-- Get the news -->
+<?php get_template_part('template-parts/blog-posts'); ?>
